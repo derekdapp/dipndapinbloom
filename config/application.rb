05dd2107efda8707deb5a032f772ed2da51777b8
derekdapp <derekdapp@gmail.com>
2015-03-12 17:02:11 -0600

    fixed env error

--- config/application.rb
+++ config/application.rb
@@ -5,14 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 
 module Dipndap
   class Application < Rails::Application
-    Bundler.require(*Rails.groups)
-
-    Dotenv::Railtie.load
-
-    HOSTNAME = ENV['HOSTNAME']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

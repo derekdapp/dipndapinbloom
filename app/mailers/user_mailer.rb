d18ef0d590d21a8f35a8d0eae7ec956048227103
derekdapp <derekdapp@gmail.com>
2015-03-16 17:16:07 -0600

    from in mailer user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "noreply@dipndap.com"
+  default :from => "Janessa <noreply@dipndap.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:

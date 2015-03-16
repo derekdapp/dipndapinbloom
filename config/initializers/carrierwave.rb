3a63238ba46d3926d5120998037bf2f2df11647e
derekdapp <derekdapp@gmail.com>
2015-03-16 15:55:26 -0600

    s3 bucket with fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
     aws_access_key_id:      ENV['S3_KEY'],
-    aws_secret_access_key:  ENV['S3_SECRET'],
+    aws_secret_access_key:  ENV['S3_SECRET']
   }
 
   # For testing, upload files to local `tmp` folder.
